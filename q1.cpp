ff9b2ff8d97a6b1301397cfc8dc12d53a01e7af3
bonbonbontt <wwwhatnowww@gmail.com>
2016-08-29 20:56:13 -0500

    [CS 225]Daily Exercise#1

---
+++ q1.cpp
@@ -0,0 +1,14 @@
+//Author: Yitong Song
+//Date: 08/29/2016
+//Daily Exercise #1
+#include <iostream>
+using namespace std;
+int main()
+{
+	int age;
+	string name;
+	age = -1;
+	name = "Sherry Song";
+	cout<<"Hello world! My name is "<<name<<" and I am "<< age <<" year old."<<endl;
+	return 0; 
+}
